8ac91f8a04941bf0e814956b41c76b40a023ed42
Jim Ng <jimng2011@gmail.com>
2017-11-23 23:47:42 +0800

    add /noj1314

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import JobsDBHandler from './handlers/JobsDBHandler';
 import BuddhaHandler from './handlers/BuddhaHandler';
 import BibleHandler from './handlers/BibleHandler';
 import HKNoITHandler from './handlers/HKNoITHandler';
+import NoJHandler from './handlers/NoJHandler';
 
 async function start() {
     const bot = TelegramUtil.getInstance();
@@ -28,6 +29,10 @@ async function start() {
         {
             regex: new RegExp(`^/hknoit(${atBot})?\\s*$`, 'i'),
             class: HKNoITHandler
+        },
+        {
+            regex: new RegExp(`^/noj1314(${atBot})?\\s*$`, 'i'),
+            class: NoJHandler
         },
         {
             regex: new RegExp(`^/jobsdb(${atBot})?\\s+(\\w+)\\s*$`, 'i'),
