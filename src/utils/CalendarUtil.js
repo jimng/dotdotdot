861b76dd9b42f4ce97b940054e1a98ed09ec3755
Jim Ng <jimng2011@gmail.com>
2019-10-24 01:55:37 +0000

    Fix /holiday issues

--- src/utils/CalendarUtil.js
+++ src/utils/CalendarUtil.js
@@ -1,15 +1,19 @@
 import axios from 'axios';
 import moment from 'moment';
+import { tify } from 'chinese-conv';
 
 async function getHolidays(todayDate, numItems = 1) {
-    const response = (await axios('http://www.1823.gov.hk/common/ical/tc.json')).data.trim();
-    const allEvents = JSON.parse(response).vcalendar[0].vevent;
+    const response = (await axios({
+        url: 'http://www.1823.gov.hk/common/ical/tc.json',
+        responseType: 'json',
+    })).data;
+    const allEvents = response.vcalendar[0].vevent;
     const futureEvents = allEvents.filter((event) =>
         moment(event.dtstart[0]).isSameOrAfter(todayDate)
     ).slice(0, numItems);
     
     return futureEvents.map((event) => ({
-        title: event.summary,
+        title: tify(event.summary),
         date: moment(event.dtstart[0]).format('YYYY-MM-DD'),
         numDays: Math.ceil(
             moment.duration(
